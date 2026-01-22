9505daa931dae4c255d8f3b5bb236753977c3f5d
bhavik2391 <bhavikchauhan23@gmail.com>
2026-01-22 08:56:49 +0530

    commiting java files

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,24 @@
 public class helloworld {
-	public static void main(String[] args) {
+	// instance field to hold a value printed by the constructor
+	private int a;
+
+	// Public no-arg constructor delegates to the parameterized constructor
+	public helloworld() {
+		this(1);
+	}
+
+	// Parameterized constructor that sets the instance field and prints it
+	public helloworld(int a) {
+		this.a = a;
+		System.out.println(a);
+	}
+
+	public void greet() {
 		System.out.println("Hello, World!");
+	}
+
+	public static void main(String[] args) {
+		helloworld hw = new helloworld();
+		hw.greet();
 	}
 }
